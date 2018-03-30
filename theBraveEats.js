ffc8b28c532d312129d2512c33ce821e8ba77f6f
Firok <351768593@outlook.com>
2018-03-30 09:55:07 +0800

    0.2.0

--- theBraveEats.js
+++ theBraveEats.js
@@ -5,7 +5,7 @@ var BE={}; // the Brave Eats
 
     BE.infos={};
     BE.infos.name="the Brave Eats";
-    BE.infos.version="0.1.0";
+    BE.infos.version="0.2.0";
 
 
     BE.bd={}; // basic datas
@@ -46,7 +46,156 @@ var BE={}; // the Brave Eats
         this.renderPath="cItem";
         return this;
     }
+    BE.bd.Items._init=function()"use strict";
+var BE={}; // the Brave Eats
+(function(){
+    console.log("be loading");
+
+    BE.infos={};
+    BE.infos.name="the Brave Eats";
+    BE.infos.version="0.2.0";
+
+
+    BE.bd={}; // basic datas
+    BE.rd={}; // runtime datas
+    BE.funs={};
+
+    // init Item
+    BE.bd.Items={};
+    BE.bd.Items.items=[];
+    BE.bd.Items.regItem=function(itemIn)
+    {
+        BE.bd.Items.items.push(itemIn);
+    }
+    BE.bd.Items.getItem=function(idIn)
+    {
+        for(let i=0;i<BE.bd.Items.items.length;i++)
+        {
+            if(BE.bd.Items.items[i].id==idIn)
+                return BE.bd.Items.items[i];
+        }
+        return BE.bd.Items.getItem("error");
+    }
+    BE.bd.Items._cItem=function() // class Item
+    {
+        this.item=this;
+        this.id="cItem";
+        this.name="cItemName";
+        this.rarity=-1;
+        this.tip="cItemTip";
+        this.onGet=function(){;};
+        this.onCraft=function(){;};
+        this.onDrop=function(){;};
+        this.canDrop=true;
+        this.onUse=function(){;};
+        this.onUseFight=function(){;};
+        this.canUse=false;
+        this.canUseFight=false;
+
+        this.type="SYS";
+        this.getData=function(dataIn,tagIn){return undefined;};
+        this.setData=function(dataIn,tagIn,valueIn){return dataIn;};
+        this.compareDataTag=function(dataIn,tagIn,valueIn{return dataIn[tagIn]==valueIn;};
+        this.getDefaultData=function(){return {};};
+
+        this.renderPath="cItem";
+        return this;
+    }
+    BE.bd.Items._cItemStack=function(idIn,amountIn,dataIn)
+    {
+        this.itemStack=this;
+        this.id=idIn;
+        this.amount=amountIn;
+
+        if(dataIn==null || dataIn==undefined)
+            this.data=return BE.bd.Items.getItem(itemStack.id).getDefaultData();
+        else
+            this.data=dataIn;
+        this.getData=function(tagIn)
+        {
+            return BE.bd.Items.getItem(itemStack.id).getData(itemStack.data,tagIn);
+        }
+        this.setData=function(tagIn,valueIn)
+        {
+            itemStack.data=BE.bd.Items.getItem(itemStack.id).setData(itemStack.data,tagIn,valueIn);
+        }
+        this.compareDataTag=function(tagIn,valueIn)
+        {
+            return BE.bd.Items.getItem(itemStack.id).compareDataTag(tagIn,valueIn);
+        }
+       
+    }
+    BE.bd.Items._cInv=function()
+    {
+        this.inv=this;
+        this.stacks=[];
+        this.amountItem=function(idIn)
+        {
+            for(let i=0;i<stacks.length;i++)
+            {
+                if(stacks[i].id==idIn)
+                    return stacks[i].amount;
+            }
+            return 0;
+        }
+        this.hasItem=function(idIn)
+        {
+            return this.amountItem(idIn)>0;
+        }
+// addItem removeItem setData getData
+        this.addItem=function(idIn,amountIn,dataIn)
+        {
+            if(this.hasItem(idIn))
+                for(let i=0;i<this.stacks.length;i++)
+                {
+                    if(this.stacks[i].id==idIn && this.stacks[i].data==dataIn)
+                }
+            else
+            {}
+        }
+        this.removeItem=function(idIn,amountIn,dataIn)
+        {
+            ;
+        }
+        this.removeItemLoc=function(locIn)
+        {
+            if(locIn<0 || locIn>=this.stacks.length)
+                return;
+            else
+                delete this.stacks[locIn];
+        }
+        this.getDataLoc=function(locIn,tagIn)
+        {
+            if(locIn<0 || locIn>=this.stacks.length)
+                return;
+            else
+                this.stacks[locIn].getData(tagIn);
+        }
+        this.setDataLoc=function(locIn,tagIn,valueIn)
+        {
+            if(locIn<0 || locIn>=this.stacks.length)
+                return;
+            else
+                this.stacks[locIn].setData(tagIn,valueIn);
+        }
+    }
     BE.bd.Items._init=function()
+    {
+        // add all items here
+        let ErrorItem=new BE.bd.Items._cItem();
+        ErrorItem.id="error";
+        ErrorItem.name="ERROR Item";
+        ErrorItem.tip="You should not get this item!";
+
+        BE.bd.Items.regItem(ErrorItem);
+    }
+
+    BE._init=function()
+    {
+        BE.bd.Items._init();
+    }
+    BE._init();
+})();
     {
         // add all items here
         let ErrorItem=new BE.bd.Items._cItem();
